774a4478d33464767235ea2e9b6054b8eb7995b1
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2017-05-02 08:04:50 -0700

    ran go fmt on loops-and-functions exercise solution

--- gotour-exercises/loops-and-functions.go
+++ gotour-exercises/loops-and-functions.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-  "fmt"
-  "math"
+	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
-  z := 1.0
-  for n := 0; n < 10; n++ {
-    z = z - ((z*z - x) / (2 * z))
-  }
-  return z
+	z := 1.0
+	for n := 0; n < 10; n++ {
+		z = z - ((z*z - x) / (2 * z))
+	}
+	return z
 }
 
 func main() {
-  const x = 2
-  mine, theirs := Sqrt(x), math.Sqrt(x)
-  fmt.Println(mine, theirs, mine-theirs)
+	const x = 2
+	mine, theirs := Sqrt(x), math.Sqrt(x)
+	fmt.Println(mine, theirs, mine-theirs)
 }
