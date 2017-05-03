245509d5c698d1ec302fbf30d47d1b4df54874f5
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2017-05-03 07:42:20 -0700

    fix missing return, run go fmt

--- gotour-exercises/stringer.go
+++ gotour-exercises/stringer.go
@@ -5,16 +5,15 @@ import "fmt"
 type IPAddr [4]byte
 
 func (addr IPAddr) String() string {
-  fmt.Sprintf("%v.%v.%v.%v", addr[0], addr[1], addr[2], addr[3])
+	return fmt.Sprintf("%v.%v.%v.%v", addr[0], addr[1], addr[2], addr[3])
 }
-// TODO: Add a "String() string" method to IPAddr
 
 func main() {
-  hosts := map[string]IPAddr{
-    "loopback": {127, 0, 0, 1},
-    "googleDNS": {8, 8, 8, 8},
-  }
-  for name, ip := range hosts {
-    fmt.Printf("%v : %v\n", name, ip)
-  }
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v : %v\n", name, ip)
+	}
 }
