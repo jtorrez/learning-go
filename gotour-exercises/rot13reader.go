f459ba3be748a1866cea0488e60be2a0b3d35cb9
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2017-05-04 08:08:38 -0700

    add rot13 reader solution, copied from solutions

---
+++ gotour-exercises/rot13reader.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func rot13(b byte) byte {
+	var a, z byte
+	switch {
+	case 'a' <= b && b <= 'z':
+		a, z = 'a', 'z'
+	case 'A' <= b && b <= 'Z':
+		a, z = 'A', 'Z'
+	default:
+		return b
+	}
+	return (b-a+13)%(z-a+1) + a
+}
+
+func (r13 rot13Reader) Read(b []byte) (int, error) {
+	n, err := r13.r.Read(b)
+	for i := 0; i < n; i++ {
+		b[i] = rot13(b[i])
+	}
+	return n, err
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
