861eb51fedf6a637d2b8fd449363184745adc4e4
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2017-05-04 15:33:21 -0700

    add goroutines example I copied

--- gotour-exercises/goroutines.go
+++ gotour-exercises/goroutines.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 )
 
 // Prints s 5 times after sleeping
 func say(s string) {
-  for i := 0; i < 5; i++ {
-    time.Sleep(100 * time.Millisecond)
-    fmt.Println(s)
-  }
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
 }
 
 func main() {
-  go say ("world")
-  say("hello")
+	go say("world")
+	say("hello")
 }
